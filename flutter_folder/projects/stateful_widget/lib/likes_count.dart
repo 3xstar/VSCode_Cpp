c5482ead7e9508bb1788d45b268cb4cc4cf965fe
3xstar <tmrniket@gmail.com>
2026-02-04 12:50:16 +0800

    c++ and flutter studying

---
+++ flutter_folder/projects/stateful_widget/lib/likes_count.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class CounterWidget extends StatefulWidget {
+  
+  const CounterWidget({super.key});
+
+  @override
+  State<CounterWidget> createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext content){
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Счетчик'),
+        backgroundColor: Colors.cyan,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Значение счетчика: ',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 10),
+
+            Text(
+              '$_counter',
+              style: const TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        backgroundColor: Colors.cyan,
+        child: const Icon(Icons.add),
+      ),
+    ),
+  );
+}
+}
